e96dc88fa0e3ecdad8aa23485cb0154ad050bd60
Snizhana-Mykhaylova <snejana.pallada@gmail.com>
2021-01-02 00:46:14 +0200

    исправляет замечания ментора

--- js/gallery.js
+++ js/gallery.js
@@ -4,32 +4,42 @@ const galleryRef = document.querySelector('.gallery.js-gallery');
 const lightboxRef = document.querySelector('.lightbox');
 const lightboxImageRef = document.querySelector('.lightbox__image');
 const lightboxOverlayRef = document.querySelector('div.lightbox__overlay');
-const btnCloseRef = document.querySelector('button[data-action= "close-lightbox"]');
+const btnCloseRef = document.querySelector(
+  'button[data-action= "close-lightbox"]',
+);
 let activIndex = 0;
 
-
-
 // генерит разметку галереи
+// gallery.map((item, index) => {
+//   const galleryItemRef = document.createElement('li');
+//   const galleryLinkRef = document.createElement('a');
+//   const galleryImageRef = document.createElement('img');
+
+//   galleryItemRef.classList.add('gallery__item');
+//   galleryLinkRef.classList.add('gallery__link');
+//   galleryImageRef.classList.add('gallery__image');
+
+//   galleryLinkRef.href = item.original;
+//   galleryImageRef.src = item.preview;
+//   galleryImageRef.alt = item.description;
+
+//   galleryImageRef.setAttribute('data-source', item.original);
+//   galleryImageRef.setAttribute('data-idx', index);
+
+//   galleryRef.appendChild(galleryItemRef);
+//   galleryItemRef.appendChild(galleryLinkRef);
+//   galleryLinkRef.appendChild(galleryImageRef);
+// });
+
 gallery.map((item, index) => {
-  const galleryItemRef = document.createElement('li');
-  const galleryLinkRef = document.createElement('a');
-  const galleryImageRef = document.createElement('img');
-  
-  
-  galleryItemRef.classList.add('gallery__item');
-  galleryLinkRef.classList.add('gallery__link');
-  galleryImageRef.classList.add('gallery__image');
-
-  galleryLinkRef.href = item.original;
-  galleryImageRef.src = item.preview;
-  galleryImageRef.alt = item.description;
-
-  galleryImageRef.setAttribute('data-source', item.original);
-  galleryImageRef.setAttribute('data-idx', index);
-
-  galleryRef.appendChild(galleryItemRef);
-  galleryItemRef.appendChild(galleryLinkRef);
-  galleryLinkRef.appendChild(galleryImageRef);
+  galleryRef.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__item">
+    <a class="gallery__link" src = "${item.original}">
+    <img class = "gallery__image" src = "${item.original}" alt = "${item.description}" data-source = "${item.original}" data-idx = "${index}"/>
+    </a>
+    </li>`,
+  );
 });
 
 // открытие модалки при клике на изображение
@@ -69,15 +79,11 @@ window.addEventListener('keydown', event => {
 });
 
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо"
-window.addEventListener('keydown', event => {
-  if (event.key === 'ArrowRight') {
-    if (activIndex >= 0 && activIndex < gallery.length - 1) {
-      activIndex = Number(activIndex) + 1;
-      lightboxImageRef.src = gallery[activIndex].original;
-    }
-    return;
-  }
+window.addEventListener('keydown', ArrowLeft);
 
+window.addEventListener('keydown', ArrowRight);
+
+function ArrowLeft(event) {
   if (event.key === 'ArrowLeft') {
     if (activIndex > 0 && activIndex <= gallery.length - 1) {
       activIndex = Number(activIndex) - 1;
@@ -85,5 +91,14 @@ window.addEventListener('keydown', event => {
     }
     return;
   }
-});
+}
 
+function ArrowRight(event) {
+  if (event.key === 'ArrowRight') {
+    if (activIndex >= 0 && activIndex < gallery.length - 1) {
+      activIndex = Number(activIndex) + 1;
+      lightboxImageRef.src = gallery[activIndex].original;
+    }
+    return;
+  }
+}
